ea9c1224539de0d28870d5afb2a1fe7016f555ac
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2021-05-05 20:35:48 +0200

    Add page loader

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 
-/* ---------- Togller Navbar --------- */
+window.addEventListener("load", () =>{
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    /* ---------- Page Loader --------- */
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(() =>{
+        document.querySelector(".page-loader").style.display = "none";
+    }, 600);
+})
+
+
+/* ---------- Toglle Navbar --------- */
 const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () => {
     hideSection();
