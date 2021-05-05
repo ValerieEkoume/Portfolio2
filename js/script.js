64a6e404657ec6019f45291622e3c5afb15922ae
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2021-05-05 18:53:22 +0200

    Create link to the section

--- js/script.js
+++ js/script.js
@@ -15,6 +15,26 @@ function toggleNavBar() {
 
 /* ---------- Active Section --------- */
 
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        navToggler.classList.add("hide");
+       if(e.target.classList.contains("nav-item")) {
+           toggleNavBar();
+       }
+       else {
+           hideSection();
+           document.body.classList.add("hide-scrolling");
+       }
+       setTimeout(() =>{
+           document.querySelector("section.active").classList.remove("active", "fade-out");
+           document.querySelector( e.target.hash).classList.add("active");
+           window.scrollTo(0,0);
+           document.body.classList.remove("hide-scrolling");
+           navToggler.classList.remove("hide");
+       }, 500);
+    }
+})
+
 /* ---------- About tab --------- */
 
 const tabsContainer = document.querySelector(".about-tabs"),
